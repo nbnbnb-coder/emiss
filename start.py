8b765a711ba4a397031b7b35860174fd4b60cb9f
nbnbnb-coder <mitalebi2009@gmail.com>
2026-05-26 21:17:49 +0330

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,7 @@
 import os
 import threading
 from flask import Flask
-
-import main
-from funcs import bot
+from database import init_db
 
 app = Flask(__name__)
 
@@ -12,10 +10,11 @@ def home():
     return "Bot is running on Render!"
 
 def run_bot():
+    import main
+    from funcs import bot
     bot.run()
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot, daemon=True).start()
-
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
